e96e19f83b246714e5264966c64ff34e1bafa9a9
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-08-18 18:50:16 -0400

    template base

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
  Blog template Jinja and Flask 
 
 ## Resources
-- Rendering Templates with Flask
+- Rendering Templates with Flask:
 https://flask.palletsprojects.com/en/1.1.x/quickstart/#rendering-templates
-- Jinja Documentation
-https://jinja.palletsprojects.com/en/2.11.x/templates/
+- Jinja Documentation:
+https://jinja.palletsprojects.com/en/2.11.x/templates
+- Npoint example blog data: 
+https://www.npoint.io/docs
--- server.py
+++ server.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+from post import Post
+import datetime
+
+current_year = datetime.date.today().year
+posts = requests.get('https://api.npoint.io/dba21db358a3ffda830f').json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post['id'], post['title'], post['subtitle'],post['body'])
+    post_objects.append(post_obj)
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def get_all_posts():
+    return render_template('index.html', all_posts = post_objects, year=current_year)
+
+@app.route('/post/<int:index>')
+def get_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template('post.html', post=requested_post, year=current_year)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
