6d92f4e98e1954b12e0ff82e9ed7f65f8f761e69
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-08-21 15:49:50 -0400

    update template

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 from flask import Flask, render_template
 import requests
-from post import Post
 import datetime
 
 current_year = datetime.date.today().year
-posts = requests.get('https://api.npoint.io/dba21db358a3ffda830f').json()
-post_objects = []
-for post in posts:
-    post_obj = Post(post['id'], post['title'], post['subtitle'],post['body'])
-    post_objects.append(post_obj)
+posts = requests.get('https://api.npoint.io/e85468e122a65f59873b').json()
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_all_posts():
-    return render_template('index.html', all_posts = post_objects, year=current_year)
+    return render_template('index.html', all_posts = posts, year=current_year)
 
 @app.route('/post/<int:index>')
-def get_post(index):
+def show_post(index):
     requested_post = None
-    for blog_post in post_objects:
+    for blog_post in posts:
         if blog_post.id == index:
             requested_post = blog_post
     return render_template('post.html', post=requested_post, year=current_year)
+
+@app.route('/about')
+def about():
+    return render_template('about.html',year=current_year)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', year=current_year)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
